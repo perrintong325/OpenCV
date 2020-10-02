be5d3553b91c7cdd28b19c986016c39ff7e0ec11
perrintong325 <perrintong@icloud.com>
2020-10-02 10:45:58 +0100

    Initial Commit

---
+++ OpenCV/OpenCV.h
@@ -0,0 +1,12 @@
+//
+//  OpenCV.h
+//  OpenCV
+//
+//  Created by Perrin Tong on 2/10/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OpenCV : NSObject
+
+@end
---
+++ OpenCV/OpenCV.m
@@ -0,0 +1,12 @@
+//
+//  OpenCV.m
+//  OpenCV
+//
+//  Created by Perrin Tong on 2/10/2020.
+//
+
+#import "OpenCV.h"
+
+@implementation OpenCV
+
+@end
